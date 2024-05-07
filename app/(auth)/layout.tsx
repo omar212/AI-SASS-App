bbc906eab28cce3f5f1ad8650c47fbf78047b6fe
Omar Elnagdy <omarelnagdy16@gmail.com>
2024-05-07 02:14:17 -0400

    removed sidebar from auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,10 +7,9 @@ const AuthLayout = ({
 }) => {
     return (
         <div className="flex items-center justify-center h-full ">
-            <Sidebar />
             {children}
         </div>
     );
 }
  
-export default AuthLayout;
+export default AuthLayout; 
